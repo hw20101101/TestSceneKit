dfc490dd99ea97588ace436614239fd99d633fd1
huangwei <huangwei@aiitec.org>
2018-12-26 12:26:17 +0800

    修改手势：1个手指旋转，2个手指移动

--- TestSceneKit/ViewController/GameViewController.m
+++ TestSceneKit/ViewController/GameViewController.m
@@ -31,7 +31,15 @@
 /**
  上一次平移的位置
  */
-@property (assign, nonatomic) CGPoint previousLoc;
+@property (assign, nonatomic) CGPoint previousMoveLoc;
+
+
+/**
+ 上一次旋转的位置
+ */
+@property (assign, nonatomic) CGPoint previousRotateLoc;
+
+@property (assign, nonatomic) int touchCount;
 
 @end
 
@@ -40,7 +48,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.previousLoc = CGPointZero;
+    //self.previousMoveLoc = CGPointZero;
     [self createSenceView];
     [self createScene];
     //[self createBox];
@@ -64,7 +72,7 @@
     //添加手势
     [self.scnView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)]];
     [self.scnView addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchGesture:)]];
-    [self.scnView addGestureRecognizer:[[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotationGesture:)]];
+    //[self.scnView addGestureRecognizer:[[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotationGesture:)]];
 }
 
 - (void)createBox{
@@ -128,10 +136,35 @@
     */
 }
 
-#pragma mark - 平移手势
+#pragma mark - 单指平移
+//- (void)panGesture:(UIPanGestureRecognizer *)pan {
+//
+//    CGPoint delta;
+//    CGPoint location = [pan locationInView:self.scnView];
+//    NSArray *hitResults = [self.scnView hitTest:location options:nil];
+//
+//    // 保证至少有一个节点被触发
+//    if([hitResults count] > 0){
+//        // 检索触发的第一个对象
+//        SCNHitTestResult *result = [hitResults objectAtIndex:0];
+//        SCNNode *geometryNode = result.node;
+//
+//        if (pan.state == UIGestureRecognizerStateChanged) {
+//            delta = CGPointMake(2 * (location.x - self.previousMoveLoc.x), 2 * (location.y - self.previousMoveLoc.y));
+//            CGFloat x = geometryNode.position.x + (CGFloat)(delta.x * 0.02);
+//            CGFloat y = geometryNode.position.y + (CGFloat)(-delta.y * (0.02));
+//            geometryNode.position = SCNVector3Make(x, y, 0);
+//            self.previousMoveLoc = location;
+//        }
+//
+//        self.previousMoveLoc = location;
+//    }
+//}
+
+#pragma mark - 1个手指旋转，2个手指移动
 - (void)panGesture:(UIPanGestureRecognizer *)pan {
 
-    CGPoint delta;
+    CGPoint delta = [pan translationInView:self.scnView];
     CGPoint location = [pan locationInView:self.scnView];
     NSArray *hitResults = [self.scnView hitTest:location options:nil];
 
@@ -141,15 +174,49 @@
         SCNHitTestResult *result = [hitResults objectAtIndex:0];
         SCNNode *geometryNode = result.node;
 
-        if (pan.state == UIGestureRecognizerStateChanged) {
-            delta = CGPointMake(2 * (location.x - self.previousLoc.x), 2 * (location.y - self.previousLoc.y));
-            CGFloat x = geometryNode.position.x + (CGFloat)(delta.x * 0.02);
-            CGFloat y = geometryNode.position.y + (CGFloat)(-delta.y * (0.02));
-            geometryNode.position = SCNVector3Make(x, y, 0);
-            self.previousLoc = location;
-        }
+        if (pan.state == UIGestureRecognizerStateBegan) {
+            self.previousRotateLoc = location;
+            self.touchCount = (int)pan.numberOfTouches;
 
-        self.previousLoc = location;
+        } else if (pan.state == UIGestureRecognizerStateChanged) {
+            delta = CGPointMake(location.x - self.previousRotateLoc.x, location.y - self.previousRotateLoc.y);
+            self.previousRotateLoc = location;
+
+            if (self.touchCount != (int)pan.numberOfTouches) {
+                return;
+            }
+
+            SCNMatrix4 rotMat;
+            if (self.touchCount == 2) { //2个手指移动
+                rotMat = SCNMatrix4MakeTranslation(delta.x * 0.025, delta.y * -0.025, 0);
+            } else { //1个手指旋转
+                SCNMatrix4 rotMatX = SCNMatrix4Rotate(SCNMatrix4Identity, (1.0f/100) * delta.y , 1, 0, 0);
+                SCNMatrix4 rotMatY = SCNMatrix4Rotate(SCNMatrix4Identity, (1.0f/100) * delta.x , 0, 1, 0);
+                rotMat = SCNMatrix4Mult(rotMatX, rotMatY);
+            }
+
+            //get the translation matrix of the child node
+            SCNMatrix4 transMat = SCNMatrix4MakeTranslation(geometryNode.position.x, geometryNode.position.y, geometryNode.position.z);
+            //move the child node to the origin of its parent (but keep its local rotation)
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform, SCNMatrix4Invert(transMat));
+            //apply the "rotation" of the parent node extra
+            SCNMatrix4 parentNodeTransMat = SCNMatrix4MakeTranslation(geometryNode.parentNode.worldPosition.x, geometryNode.parentNode.worldPosition.y, geometryNode.parentNode.worldPosition.z);
+            SCNMatrix4 parentNodeMatWOTrans = SCNMatrix4Mult(geometryNode.parentNode.worldTransform, SCNMatrix4Invert(parentNodeTransMat));
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform, parentNodeMatWOTrans);
+
+            //apply the inverse "rotation" of the current camera extra
+            SCNMatrix4 camorbitNodeTransMat = SCNMatrix4MakeTranslation(self.scnView.pointOfView.worldPosition.x, self.scnView.pointOfView.worldPosition.y, self.scnView.pointOfView.worldPosition.z);
+            SCNMatrix4 camorbitNodeMatWOTrans = SCNMatrix4Mult(self.scnView.pointOfView.worldTransform, SCNMatrix4Invert(camorbitNodeTransMat));
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform,SCNMatrix4Invert(camorbitNodeMatWOTrans));
+            //perform the rotation based on the pan gesture
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform, rotMat);
+            //remove the extra "rotation" of the current camera
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform, camorbitNodeMatWOTrans);
+            //remove the extra "rotation" of the parent node (we can use the transform because parent node is at world origin)
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform,SCNMatrix4Invert(parentNodeMatWOTrans));
+            //add back the local translation mat
+            geometryNode.transform = SCNMatrix4Mult(geometryNode.transform, transMat);
+        }
     }
 }
 
@@ -177,7 +244,7 @@
 #pragma mark - 旋转手势
 - (void)rotationGesture:(UIRotationGestureRecognizer *)rotation {
 
-    /* TODO 
+    /*
     // 检查触发了哪些节点
     CGPoint p = [rotation locationInView:self.scnView];
     NSArray *hitResults = [self.scnView hitTest:p options:nil];
@@ -193,39 +260,39 @@
 }
 
 //单指 翻转、旋转，方式一
-- (void)rotate:(UIPanGestureRecognizer *)pan geometryNode:(SCNNode *)geometryNode{
-
-    CGPoint translation = [pan translationInView:pan.view];
-    CGFloat newAngleX = (CGFloat)(translation.y)*(CGFloat)(M_PI)/180.0;
-    newAngleX += self.currentAngleX;
-    CGFloat newAngleY = (CGFloat)(translation.x)*(CGFloat)(M_PI)/180.0;
-    newAngleY += self.currentAngleY;
-    geometryNode.eulerAngles = SCNVector3Make(newAngleX, newAngleY, 0);
-
-    if (pan.state == UIGestureRecognizerStateEnded) {
-        self.currentAngleX = newAngleX;
-        self.currentAngleY = newAngleY;
-    }
-}
+//- (void)rotate:(UIPanGestureRecognizer *)pan geometryNode:(SCNNode *)geometryNode{
+//
+//    CGPoint translation = [pan translationInView:pan.view];
+//    CGFloat newAngleX = (CGFloat)(translation.y)*(CGFloat)(M_PI)/180.0;
+//    newAngleX += self.currentAngleX;
+//    CGFloat newAngleY = (CGFloat)(translation.x)*(CGFloat)(M_PI)/180.0;
+//    newAngleY += self.currentAngleY;
+//    geometryNode.eulerAngles = SCNVector3Make(newAngleX, newAngleY, 0);
+//
+//    if (pan.state == UIGestureRecognizerStateEnded) {
+//        self.currentAngleX = newAngleX;
+//        self.currentAngleY = newAngleY;
+//    }
+//}
 
 //单指 翻转、旋转，方式二
-- (void)rotate2:(UIPanGestureRecognizer *)pan geometryNode:(SCNNode *)geometryNode{
-
-    CGPoint translation = [pan translationInView:pan.view];
-    CGFloat x = (CGFloat)(translation.x);
-    CGFloat y = (CGFloat)(-translation.y);
-
-    CGFloat anglePan = sqrt(pow(x,2) + pow(y,2)) * (CGFloat)(M_PI)/180.0;
-    SCNVector4 rotationVector = SCNVector4Make(-y, x, 0, anglePan);
-    geometryNode.rotation = rotationVector;
-
-    if (pan.state == UIGestureRecognizerStateEnded) {
-        SCNMatrix4 currentPivot = geometryNode.pivot;
-        SCNMatrix4 changePivot = SCNMatrix4Invert(geometryNode.transform);
-        geometryNode.pivot = SCNMatrix4Mult(changePivot, currentPivot);
-        geometryNode.transform = SCNMatrix4Identity;
-    }
-}
+//- (void)rotate2:(UIPanGestureRecognizer *)pan geometryNode:(SCNNode *)geometryNode{
+//
+//    CGPoint translation = [pan translationInView:pan.view];
+//    CGFloat x = (CGFloat)(translation.x);
+//    CGFloat y = (CGFloat)(-translation.y);
+//
+//    CGFloat anglePan = sqrt(pow(x,2) + pow(y,2)) * (CGFloat)(M_PI)/180.0;
+//    SCNVector4 rotationVector = SCNVector4Make(-y, x, 0, anglePan);
+//    geometryNode.rotation = rotationVector;
+//
+//    if (pan.state == UIGestureRecognizerStateEnded) {
+//        SCNMatrix4 currentPivot = geometryNode.pivot;
+//        SCNMatrix4 changePivot = SCNMatrix4Invert(geometryNode.transform);
+//        geometryNode.pivot = SCNMatrix4Mult(changePivot, currentPivot);
+//        geometryNode.transform = SCNMatrix4Identity;
+//    }
+//}
 
 #pragma mark - 获取被点击的节点
 - (void) handleTap:(UIGestureRecognizer*)gestureRecognize
